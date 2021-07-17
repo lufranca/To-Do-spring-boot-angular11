a4e4dc305d17227d07c9e1eb50d79529e6862715
Luciano de França <lucyano_spfc@hotmail.com>
2021-07-17 18:00:37 -0300

    Iniciando projeto To-Do

---
+++ src/test/java/com/luciano/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
